a8467ff18fc23867f3ac5d48a8fe513660b20545
Miroslav Pašek <miroslav.pasek@email.cz>
2021-02-15 23:31:16 +0100

    UI done

--- src/app/budget-item-list/budget-item-card/budget-item-card.component.ts
+++ src/app/budget-item-list/budget-item-card/budget-item-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-budget-item-card',
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BudgetItemCardComponent implements OnInit {
 
-  constructor() { }
+  @Input() isIncome = true;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
